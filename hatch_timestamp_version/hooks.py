bd62012bee6d6a59f26c5ca50348453ab573c071
Visesh Prasad <viseshrprasad@gmail.com>
2025-05-26 14:01:32 -0400

    Update hooks.py

--- hatch_timestamp_version/hooks.py
+++ hatch_timestamp_version/hooks.py
@@ -1,12 +1,12 @@
+from hatch_vcs.build_hook import VCSBuildHook
+from hatch_vcs.metadata_hook import VCSMetadataHook
+from hatch_vcs.version_source import VCSVersionSource
 from hatchling.builders.hooks.plugin.interface import BuildHookInterface
 from hatchling.metadata.plugin.interface import MetadataHookInterface
 from hatchling.plugin import hookimpl
 from hatchling.version.scheme.plugin.interface import VersionSchemeInterface
 from hatchling.version.source.plugin.interface import VersionSourceInterface
 
-from hatch_vcs.build_hook import VCSBuildHook
-from hatch_vcs.metadata_hook import VCSMetadataHook
-from hatch_vcs.version_source import VCSVersionSource
 from .ts_scheme import TimestampDevVersionScheme
 
 
