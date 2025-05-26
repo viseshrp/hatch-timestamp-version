4ab5f7b376121da5bc37cdd489036872dcc1d5dc
Visesh Prasad <viseshrprasad@gmail.com>
2025-05-26 13:24:44 -0400

    fix types

--- hatch_timestamp_version/hooks.py
+++ hatch_timestamp_version/hooks.py
@@ -1,5 +1,8 @@
+from hatchling.builders.hooks.plugin.interface import BuildHookInterface
+from hatchling.metadata.plugin.interface import MetadataHookInterface
 from hatchling.plugin import hookimpl
 from hatchling.version.scheme.plugin.interface import VersionSchemeInterface
+from hatchling.version.source.plugin.interface import VersionSourceInterface
 
 from ._vendor.hatch_vcs.build_hook import VCSBuildHook
 from ._vendor.hatch_vcs.metadata_hook import VCSMetadataHook
@@ -8,17 +11,17 @@ from .ts_scheme import TimestampDevVersionScheme
 
 
 @hookimpl
-def hatch_register_version_source():
+def hatch_register_version_source() -> type[VersionSourceInterface]:
     return VCSVersionSource
 
 
 @hookimpl
-def hatch_register_build_hook():
+def hatch_register_build_hook() -> type[BuildHookInterface]:
     return VCSBuildHook
 
 
 @hookimpl
-def hatch_register_metadata_hook():
+def hatch_register_metadata_hook() -> type[MetadataHookInterface]:
     return VCSMetadataHook
 
 
